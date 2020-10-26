78e8d94a65596e33201e3334383bc832c5e278b1
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2020-10-26 08:49:07 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Github and Maven" );
+        System.out.println( "Hello From my project" );
     }
 }
