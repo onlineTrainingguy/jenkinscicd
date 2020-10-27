d3de7bf18b50534fbee62353b2ca13f149cf7a00
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2020-10-27 08:23:55 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello From my project" );
+        System.out.println( "Hello From CI CD" );
     }
 }
