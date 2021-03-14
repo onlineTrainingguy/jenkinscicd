5a6c377ab8284406be66365caa27311c6e66c6d8
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2021-03-14 20:14:04 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Jenkins Developer!" );
+        System.out.println( "Maven Job is running on Build Server!" );
     }
 }
