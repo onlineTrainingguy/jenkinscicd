4ec2b6f24451cccecf2ddec77e78d28a0e4c0f6f
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2022-08-17 08:44:13 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Testing in progress" );
+        System.out.println( "Testing in progress- Build Pipeline" );
     }
 }
