af54cf3689878eb48e2320efc86c34bac72029ec
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2022-10-03 16:02:32 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Testing in progress- Build Pipeline- by Automation Process done by Poll Scm" );
+        System.out.println( "Testing in progress- Build Pipeline- by Automation Process done-Docker" );
     }
 }
