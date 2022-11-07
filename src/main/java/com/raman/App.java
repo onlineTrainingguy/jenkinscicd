9299634621d654a0bdeba44f580121c4d95c1747
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2022-11-07 11:05:24 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Poll Scm Testing" );
+        System.out.println( "Webhook - Poll Scm Testing" );
     }
 }
