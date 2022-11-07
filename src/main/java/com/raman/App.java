4a5f1c62c5aec4640292b6cb5798a05f214d840d
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2022-11-07 10:57:35 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Testing in progress- Build Pipeline- by Automation Process done-Docker Webhook-12345" );
+        System.out.println( "Poll Scm Testing" );
     }
 }
