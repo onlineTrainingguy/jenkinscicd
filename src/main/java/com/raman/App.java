be94f1dd70d9711a925335d570bdb9bc7c0685b7
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2022-12-11 10:39:18 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Maven Testing008 " );
+        System.out.println( "Maven Testing009 " );
     }
 }
