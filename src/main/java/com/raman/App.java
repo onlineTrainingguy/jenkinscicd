528a3e6e93a864ff8c59efa4fb7c719cacc0a956
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2023-03-27 15:50:21 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Maven Testing010 " );
+        System.out.println( "Jenkins job testing" );
     }
 }
