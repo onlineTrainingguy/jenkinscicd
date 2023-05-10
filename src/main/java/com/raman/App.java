baf731ac6cadadcddc71cb6654499e1b98b651a8
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2023-05-10 17:24:07 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Jenkins job testing with Maven-6" );
+        System.out.println( "Jenkins job testing with Maven-7" );
     }
 }
