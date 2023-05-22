ae5a1f9fc0b0d42f710e4307ca8dfe321e402b11
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2023-05-22 15:47:22 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from Jenkins-1" );
+        System.out.println( "Hello from Jenkins-2" );
     }
 }
