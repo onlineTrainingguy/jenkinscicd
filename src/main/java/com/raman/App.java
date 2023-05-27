621e9d8dab0ff9f7ab9cd0f162174f6f0f8c1c72
globaluser <globaluser@gmail.com>
2023-05-27 22:53:49 +0530

    Commit to my project to build automatcially

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from Jenkins-6" );
+        System.out.println( "Welcome to Banking Project!!!" );
     }
 }
