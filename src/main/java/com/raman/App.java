6b152a012933d5aaa03955c9375e934d6ec1825e
globaluser <globaluser@gmail.com>
2023-05-27 22:58:58 +0530

    new version of my app

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Banking Project!!!" );
+        System.out.println( "Welcome to CITI Banking Project!!!" );
     }
 }
