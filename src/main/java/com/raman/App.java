825d0f6e3ae55f239020fc24650b9e56287fc463
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2023-06-16 17:32:13 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to CITI Banking Project-3!!!" );
+        System.out.println( "Welcome to CITI Banking Project-4!!!" );
     }
 }
