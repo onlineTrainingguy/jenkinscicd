b3bee3e785b3bf0ba8e176c27de38628de1557e7
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2023-07-11 16:13:27 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Allianz Project-For France Region!" );
+        System.out.println( "Welcome to Allianz Project-For France Region with DevOps Team " );
     }
 }
