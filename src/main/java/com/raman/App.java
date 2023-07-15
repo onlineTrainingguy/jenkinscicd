b8e3cacfa1c999cb1b3470570f4df6168cdbf76a
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2023-07-15 21:01:39 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Jenkins Project " );
+        System.out.println( "Welcome to Jenkins Project-123 " );
     }
 }
