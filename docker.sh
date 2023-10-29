1e2cbe8838a512e84920a7a130c303dcbed07612
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2023-10-29 09:07:40 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ then
         sudo docker rm -f $(sudo docker ps -a -q)
 fi
 count1=`sudo docker images | wc -l`
-if [ $count -gt 1 ]
+if [ $count1 -gt 1 ]
 then
         sudo docker rmi -f $(sudo docker images -q)
 fi
