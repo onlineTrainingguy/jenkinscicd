1b85f6b6661919eed466819d3c24092074d3fe76
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2023-10-29 08:56:14 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,10 @@
+count=`sudo docker ps -a | wc -l`
+if [ $count -gt 1 ]
+then
+        sudo docker rm -f $(sudo docker ps -a -q)
+fi
+count1=`sudo docker images | wc -l`
+if [ $count -gt 1 ]
+then
+        sudo docker rmi -f $(sudo docker images -q)
+fi
