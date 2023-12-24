a897cc6ab760db79105055fae7261885ee23550c
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2023-12-24 10:26:53 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Jenkins Project for  Genius Morning Batch 8" );
+        System.out.println( "Welcome to Jenkins Project for  Genius Morning Batch 9" );
     }
 }
