7c1ae998789e1d5f36ec68fc18674207c7a11ad0
onlineTrainingguy <47533047+onlineTrainingguy@users.noreply.github.com>
2024-02-01 10:57:53 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Jenkins Project for  Genius Allianz Batch DevOps" );
+        System.out.println( "Welcome to Jenkins Project for  Genius Allianz Batch DevOps Morning" );
     }
 }
