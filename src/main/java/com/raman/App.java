1c3deba30186548d0573f3f8d9238e5d173acb07
Raman <47533047+onlineTrainingguy@users.noreply.github.com>
2025-04-17 12:42:08 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Jenkins Project for  Genius Morning DevOps Batch -14" );
+        System.out.println( "Welcome to Jenkins Project for  Genius Morning DevOps Batch -15" );
     }
 }
