2d3f606b45cc4457aa9dabae37bee49addf77bff
Raman <47533047+onlineTrainingguy@users.noreply.github.com>
2025-09-01 10:28:02 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Jenkins" );
+        System.out.println( "Hello cloud Jenkins" );
     }
 }
